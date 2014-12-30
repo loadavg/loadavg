247c97f814f8ba92f65a449b89138c7289c034cc
Karsten Becker <k.becker@sputnik7.com>
2014-12-30 17:07:50 -0500

    fixed autoreload in header

--- app/layout/header.php
+++ app/layout/header.php
@@ -141,7 +141,7 @@ if (isset($_POST['login'])  ) {
 	if ( (!isset($_GET['minDate'])) || (!isset($_GET['maxDate'])) || (!isset($_GET['logdate'])) ) 
 	{ 
 		//if (    ($settings['settings']['title'] == "true") )
-		if (   (isset($settings['settings']['auto_refreash']))  && ($settings['settings']['auto_refreash'] == "true") )
+		if (   (isset($settings['settings']['autoreload']))  && ($settings['settings']['autoreload'] == "true") )
 		{
 		?>
 		<meta http-equiv="refresh" content="300">
